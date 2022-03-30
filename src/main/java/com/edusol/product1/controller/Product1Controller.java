6a9da4e9f8148fc34870c078ba7955e5708384a6
Hari <Hari@Hari>
2022-03-30 09:46:34 +0530

    added log into add product

--- src/main/java/com/edusol/product1/controller/Product1Controller.java
+++ src/main/java/com/edusol/product1/controller/Product1Controller.java
@@ -35,6 +35,7 @@ public class Product1Controller {
 
 	@PostMapping("/add-product")
 	public Object addProduct(@RequestBody Product1 product1) {
+		
 		logger.info(product1.toString());
 		return product1Service.addProduct(product1);
 	}
